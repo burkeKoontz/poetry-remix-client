7217ecc815bd74831ba53f0540a1f1476ab24d59
Burkey Koontz <bk.koontz@gmail.com>
2018-09-10 07:20:26 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CheeseList from './components/cheese-list';
+import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import {cheeseReducer} from './reducers/cheese';
+import {Provider} from 'react-redux';
+
+const store = createStore(cheeseReducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}>
+<CheeseList />
+</Provider>, document.getElementById('root'));
+registerServiceWorker();
